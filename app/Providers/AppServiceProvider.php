c8f33bb04b7453536186ed454a534fabe50b0bd9
Osama <osamazafar2332@gmail.com>
2026-03-18 20:47:45 +0500

    update config/app and providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+                \Illuminate\Support\Facades\URL::forceScheme('https');
+            }
+
     // Load API routes
         Route::middleware('api')
             ->prefix('api') // optional, all routes will start with /api
--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://trial-production-147b.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
